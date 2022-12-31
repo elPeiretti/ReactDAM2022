fd56228b4552705a0b8ba2c4c80fb93bb906f92a
elPeiretti <tomaspeiretti@gmail.com>
2022-12-31 00:27:30 -0300

    se agrega boton para editar

--- componentes/EditarScreen.js
+++ componentes/EditarScreen.js
@@ -5,7 +5,7 @@ import ItemTarea from './ItemTarea';
 
 function EditarScreen() {
 
-  const [tareas, setTareas] = useState([{texto: "una tarea", done: true}]);
+  const [tareas, setTareas] = useState([{texto: "una tarea MUY MUY MUY MUY LARGA", done: true}]);
 
   const deleteTarea = (tareaPorEliminar) => {
     setTareas(tareas.filter((tarea) => tarea.texto != tareaPorEliminar));
@@ -18,8 +18,13 @@ function EditarScreen() {
             {tareas.map((t) => (
                 <View style={styles.container}>
                     <ItemTarea text={t.texto} isChecked={t.done}/>
+                    <TouchableOpacity onPress={()=> console.log("editar")}>
+                        <View style={styles.buttonEditar}>
+                            <Text style={{color:'#FFFFFF', fontSize: 12}}>editar</Text>
+                        </View>
+                    </TouchableOpacity>
                     <TouchableOpacity onPress={()=> deleteTarea(t.texto)}>
-                        <View style={styles.button}>
+                        <View style={styles.buttonBorrar}>
                             <Text style={{color:'#FFFFFF', fontSize: 12}}>eliminar</Text>
                         </View>
                     </TouchableOpacity>
@@ -34,7 +39,6 @@ export default EditarScreen;
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 10,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
@@ -42,12 +46,20 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         minHeight: 30,
     },
-    button:{
+    buttonBorrar:{
         height: 20,
         width: 50,
         borderRadius: 5,
         backgroundColor: '#ff5f5f',
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        justifyContent: 'center',
+    },
+    buttonEditar:{
+        height: 20,
+        width: 50,
+        borderRadius: 5,
+        backgroundColor: '#f09f48',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
