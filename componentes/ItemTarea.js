d8727cc7c436bd2ac2483b49365b0ad9f1335ef2
elPeiretti <tomaspeiretti@gmail.com>
2023-01-19 14:14:48 -0300

    cambio de BouncyCheckbox por Checkbox

--- componentes/ItemTarea.js
+++ componentes/ItemTarea.js
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, View} from "react-native";
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { StyleSheet, View, Text} from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Checkbox } from 'react-native-paper';
 
 function ItemTarea(props) {
 
     const [id,setId] = useState(props.id);
+    const [checked, setChecked] = useState(props.isChecked);
 
     const saveState = async (isChecked) => {
+        setChecked(isChecked);
         const tareaJson = JSON.stringify({text: props.text, isChecked: isChecked, key: id});
-        await AsyncStorage.setItem(id, tareaJson)
+        await AsyncStorage.setItem(id, tareaJson);
     };
 
     return (
         <View style={styles.tareaContainer}>
-            <BouncyCheckbox
-                textStyle = {{textAlign: 'justify'}}
-                text = {props.text}
-                isChecked = {props.isChecked}
-                onPress={(isChecked) => {
-                    saveState(isChecked);
-                }} 
+            <Checkbox
+                color='#f09f48'
+                status={checked ? 'checked' : 'unchecked'}
+                onPress={() => {
+                    saveState(!checked);    
+                }}  
             />
+            <Text style={styles.texto}>{props.text}</Text>
         </View>
     );
 }
@@ -39,4 +41,11 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         minHeight: 30,
     },
+    texto: {
+        textAlign: 'justify',
+        fontFamily: "JosefinSans-Regular",
+        fontSize: 16,
+        flex: 1,
+        color: '#5A5A5A'
+    }
 });
